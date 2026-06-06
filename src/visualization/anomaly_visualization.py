d9d0eb018f46fb24f45fb6024493f048a1239c4b
Parashuram <parashurampawar56@gmail.com>
2026-06-06 12:58:40 +0530

    modified program: [parashuram] updated python script to plot anomaly visualization

--- src/visualization/anomaly_visualization.py
+++ src/visualization/anomaly_visualization.py
@@ -22,19 +22,40 @@ def anomaly_visualization(df):
         :return:
             None
     """
+    df = df.sample(
+        min(1000, 
+            len(df)), 
+            random_state=42
+            )
     df = df.sort_values("timeStamp")
+
     logging.info("Plotting cTemp vs timestamp anomaly...")
     plt.figure(figsize=(8, 8))
-    plt.plot(df['timeStamp'], df['cTemp'], label='Temperature', color='blue')
+
+    plt.plot(df['timeStamp'], 
+             df['cTemp'], 
+             label='Temperature', 
+             color='blue'
+             )
+    
     anomaly_df = df[df['temp_anomaly_flag'] == 1]
-    plt.scatter( anomaly_df['timeStamp'], anomaly_df['cTemp'], label="Anomaly", marker='o', color='red')
+    plt.scatter( anomaly_df['timeStamp'], 
+                anomaly_df['cTemp'], 
+                label="Anomaly", 
+                marker='o', 
+                color='red',
+                alpha=0.5
+                )
+    
     plt.title("Temperature Anomaly Detection", fontdict=font_dict)
     plt.xlabel("Timestamp")
-    plt.ylabel("Temperature")
+    plt.ylabel("Temperature (°C)")
     plt.legend()
-
     plt.xticks(rotation=45)
-    plt.savefig("screenshots/temp_anomaly.png")
+
+    plt.savefig("screenshots/temp_anomaly.png",
+                bbox_inches='tight'
+                )
     logging.info("Temperature anomaly plot completed...")
 
 
@@ -53,14 +74,31 @@ def speed_anomaly(df):
     df = df.sort_values("timeStamp")
     logging.info("Plotting gps_speed vs timestamp anomaly...")
     plt.figure(figsize=(12, 5))
-    plt.plot(df['timeStamp'], df['gps_speed'], label='Speed', color='orange')
+
+    plt.plot(df['timeStamp'], 
+             df['gps_speed'], 
+             label='Speed', 
+             color='orange'
+             )
     spike = df[df['speed_spike'] == 1]
-    plt.scatter(spike['timeStamp'], spike['gps_speed'], label='Speed Spike', marker='o', color='blue')
+
+    plt.scatter(
+        spike['timeStamp'],
+        spike['gps_speed'],
+        color='red',
+        s=80,
+        edgecolors='black',
+        linewidth=1,
+        zorder=3
+    )
+
     plt.xlabel("Time")
     plt.ylabel("Speed")
-    plt.title("Speed Spike Detection")
+    plt.title("Vehicle Speed Spike Detection", fontdict=font_dict)
     plt.legend()
 
     plt.xticks(rotation=45)
-    plt.savefig("screenshots/speed_spike.png")
+    plt.savefig("screenshots/speed_spike.png",
+                bbox_inches='tight'
+                )
     logging.info("Speed spike plot completed...")
